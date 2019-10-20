0b35fa64b5515de317a6ed82e8452bf69236573d
Norbert Bielak <norbertbielak@gmail.com>
2019-10-20 12:30:30 +0200

    1

--- PWMlib/main.c
+++ PWMlib/main.c
@@ -1,19 +1,19 @@
 /*  Library for software and hardware PWM operating for AVR uC.
  * 	It can be used in light brightness adjustment, controlling DC motor's speed or
- * 	as in example RGB LED control
+ * 	as in example RGB LED control.
  *
- *	Allows using soft and hard PWM in inverting mode or non-inverting mode
+ *	Allows using soft and hard PWM in inverting mode or non-inverting mode.
  *
  *  I recommend using 16MHZ crystal.
  *
- *  Required use soft_pwm_init() or hard_pwm_init function before using PWM
- *  Required use sei() function in main.c
- * 	Required libraires : <avr/io.h> <avr/interrupt.h>
+ *  Required use soft_pwm_init() or hard_pwm_init function before using PWM.
+ *  Required use sei() function in main.c.
+ * 	Required libraires : <avr/io.h> <avr/interrupt.h>.
  *
- *  The library takes about 600 bytes of FLASH memory with sample program
+ *  The library takes about 600 bytes of FLASH memory with sample program.
  *  for all 4 software channels and hardware channel active.
  *
- *  MCU used in project is ATmega32
+ *  MCU used in project is ATmega32.
  *
  *  Created on: 3 oct 2019
  *      Author: Norbert Bielak
